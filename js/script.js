40f99dba935ebc7edf59d67436b5fbd566979548
ssshun3 <s.kudo.22.7.584@4nonome.com>
2024-11-10 15:27:15 +0900

    add:loading

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 window.addEventListener("DOMContentLoaded", init);
 
 function init() {
+  const loadingElement = document.createElement("div");
+  loadingElement.id = "loading";
+  loadingElement.style.position = "absolute";
+  loadingElement.style.top = "50%";
+  loadingElement.style.left = "50%";
+  loadingElement.style.transform = "translate(-50%, -50%)";
+  loadingElement.style.fontSize = "24px";
+  loadingElement.style.color = "#ffffff";
+  loadingElement.style.backgroundColor = "rgba(0, 0, 0, 0.7)";
+  loadingElement.style.padding = "20px";
+  loadingElement.style.borderRadius = "10px";
+  loadingElement.innerText = "Loading...";
+  document.body.appendChild(loadingElement);
   // レンダラーを作成
   const canvasElement = document.querySelector("#myCanvas");
   const renderer = new THREE.WebGLRenderer({
@@ -65,8 +78,13 @@ function init() {
       model.scale.set(50.0, 50.0, 50.0);
       model.position.set(0, -200, 0);
       scene.add(model);
+      loadingElement.style.display = "none"; // ロード完了で非表示
     },
-    undefined,
+    function (xhr) {
+      // ロードの進行状況
+      const progress = (xhr.loaded / xhr.total) * 100;
+      loadingElement.innerText = `Loading... ${Math.round(progress)}%`;
+    },
     function (error) {
       console.error(error);
     }
